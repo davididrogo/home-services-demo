84cb42c574e6266dcd7af91935dda4f3cc89043f
eric idrogo <eric.idrogo_consultor@mdp.com.pe>
2025-09-11 16:41:33 -0500

    test class for testing PR into main repo

---
+++ src/main/java/com/example/homesvc/config/BookingStateMachineConfig.java
@@ -0,0 +1,9 @@
+package com.example.homesvc.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookingStateMachineConfig {
+    private static final Logger log = LoggerFactory.getLogger(BookingStateMachineConfig.class);
+
+}
